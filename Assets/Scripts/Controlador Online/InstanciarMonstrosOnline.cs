d5b78d8f1f0cd2709e4a62df1db5810bdad1d9ca
Lucas Begnini <lucasbegnini@gmail.com>
2015-04-27 18:32:46 -0400

    [Multiplayer Online] Implementação dos monstros

--- Assets/Scripts/Controlador Online/InstanciarMonstrosOnline.cs
+++ Assets/Scripts/Controlador Online/InstanciarMonstrosOnline.cs
@@ -6,13 +6,30 @@ public class InstanciarMonstrosOnline : MonoBehaviour {
 	public Vector3 [] posicoes;
 	public bool [] posicoesUsadas;
 	private int posicaoEscolhida;
-	// Use this for initialization
-	void Start () {
-	
+
+
+	void OnJoinedRoom()
+	{
+		instanciarMonstros ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void instanciarMonstros()
+	{
+		for (int i = 0; i < posicoes.Length; i++) {
+			SelectPosicao();
+		//	GameObject.Instantiate(monsters[i],posicoes[posicaoEscolhida],monsters[i].transform.rotation);
+		
+		}
+	}
+
+	void SelectPosicao()
+	{
+		int rand = Random.Range (0, 12);
+		if (posicoesUsadas [rand] == false) {
+			posicaoEscolhida = rand;
+			posicoesUsadas[rand] = true;
+		} else {
+			SelectPosicao();
+		}
 	}
 }
