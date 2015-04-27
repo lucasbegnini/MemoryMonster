fdcff8896f7c278fe03214e7c1b1366532ad9184
Lucas Begnini <lucasbegnini@gmail.com>
2015-04-27 17:57:27 -0400

    [Multiplayer Online] Implementação da sala

---
+++ Assets/Scripts/Controlador Online/GeradorDeSala.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeradorDeSala : MonoBehaviour {
+
+	void Start()
+	{
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+	
+	void OnGUI()
+	{
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+	void OnJoinedLobby()
+	{
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnPhotonRandomJoinFailed()
+	{
+		Debug.Log("Can't join random room!");
+		PhotonNetwork.CreateRoom(null);
+	}
+
+
+}
